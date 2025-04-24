7af826f6066c3d6597ac1a0c78e5a2015c743058
davidrguez98 <ropeda98@gmail.com>
2025-04-24 17:28:41 +0200

    falta hacer la automaticación en el server

--- src/emailAutomation.py
+++ src/emailAutomation.py
@@ -1,25 +1,12 @@
 from newsAPI import *
-import yagmail
-from dotenv import load_dotenv
-import os
+from emailConfig import *
 
-def sendEmail():
 
-    load_dotenv(".env")
-    PASSWORD = os.getenv("PASSWORD_EMAIL")
 
-    email = "ropeda98publi@gmail.com"
+def emailAutomation():
 
-    yag = yagmail.SMTP(user=email, password=PASSWORD)
-    content = ["./email.txt"]
+    main()
+    sendEmail()
+    cleanAll()
 
-    yag.send("davidrally09@gmail.com", "GUIONES", content)
-
-def cleanAll():
-
-    JSONfile = "./data_news.json"
-    TXTfile = "./email.txt"
-
-    os.remove(JSONfile)
-    os.remove(TXTfile)
-
+emailAutomation()
---
+++ src/emailConfig.py
@@ -0,0 +1,25 @@
+from newsAPI import *
+import yagmail
+from dotenv import load_dotenv
+import os
+
+def sendEmail():
+
+    load_dotenv(".env")
+    PASSWORD = os.getenv("PASSWORD_EMAIL")
+
+    email = "ropeda98publi@gmail.com"
+
+    yag = yagmail.SMTP(user=email, password=PASSWORD)
+    content = ["./email.txt"]
+
+    yag.send("davidrally09@gmail.com", "GUIONES", content)
+
+def cleanAll():
+
+    JSONfile = "./data_news.json"
+    TXTfile = "./email.txt"
+
+    os.remove(JSONfile)
+    os.remove(TXTfile)
+
